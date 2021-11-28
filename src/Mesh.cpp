e601214de59e06e83b9696193dc15186b5ca7cd1
Jonah Taylor <jonahtaylor@att.net>
2021-11-28 00:06:32 -0600

    Combine all mesh vertex data into a single VB
    Also, switch from a hardcoded plane to a hardcoded cube

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -2,9 +2,9 @@
 
 Mesh::Mesh() :
 	vertexArrayID(0),
+	vertexBufferID(0),
 	elementBufferID(0),
-	positionBufferID(0),
-	colorBufferID(0)
+	useEBO(false)
 {
 
 }
@@ -12,53 +12,77 @@ Mesh::Mesh() :
 Mesh::~Mesh() {
 	// Deallocate all of the buffers and arrays
 	glDeleteVertexArrays(1, &vertexArrayID);
+	glDeleteBuffers(1, &vertexBufferID);
 	glDeleteBuffers(1, &elementBufferID);
-	glDeleteBuffers(1, &positionBufferID);
-	glDeleteBuffers(1, &colorBufferID);
 }
 
-void Mesh::LoadMesh(const char* filename) {
+void Mesh::LoadMesh(const char* filename, const bool use_EBO) {
 	// TODO: later, load the mesh from a file. For now, just hardcode
-	//   the mesh's points
+	//   the mesh's points as a cube
 
 	// Specify the positions in Normalized Device Coordinates (range -1 to 1,
 	//   where 0,0,0 is the center of the world)
 	// OpenGL converts this to screen-space coordinates (where 0,0 = top left)
 	//   using the info provided in glviewport commands
 	GLfloat positions[] = {
-		// Vertex data
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-	   -0.5f, -0.5f, 0.0f,  // bottom left
-	   -0.5f,  0.5f, 0.0f,  // top left 
-	};
-	positionBuffer.assign(std::begin(positions), std::end(positions));
-	// Note: You could wrap the color into the same VBO as the positions, you'd just
-	//  have to be careful when setting up the attribute pointers
-	GLfloat colors[] = {
-		1.0f, 0.0f, 0.0f,  // top right
-		0.0f, 1.0f, 0.0f,  // bottom right
-		0.0f, 0.0f, 1.0f,  // bottom left
-		0.0f, 0.0f, 0.0f   // top left 
-	};
-	colorBuffer.assign(std::begin(colors), std::end(colors));
-	// Texture coords, defined with (0,0) = bottom left
-	GLfloat tex_coords[] = {
-		1.0, 1.0,  // top right
-		1.0, 0.0,  // bottom right
-		0.0, 0.0,  // bottom left
-		0.0, 1.0   // top left
-	};
-	texCoordBuffer.assign(std::begin(tex_coords), std::end(tex_coords));
+		// Vertex data for a cube
+		//  X,     Y,     Z,  texU, texV,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-	// Tells openGL which vertices to draw in what order
-	//   (used by the Element Buffer Object)
-	GLuint indices[] = {  //  note that we start from 0!
-		0, 1, 3,   //  first triangle
-		1, 2, 3    //  second triangle
-	};
-	elementBuffer.assign(std::begin(indices), std::end(indices));
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+	vertexBuffer.assign(std::begin(positions), std::end(positions));
+
+	useEBO = use_EBO;
+	if (useEBO) {
+		// Tells openGL which vertices to draw in what order
+		//   (used by the Element Buffer Object)
+		GLuint indices[] = {  //  note that we start from 0!
+			// These are hardcoded indices for a plane
+			0, 1, 3,   //  first triangle
+			1, 2, 3    //  second triangle
+		};
+		elementBuffer.assign(std::begin(indices), std::end(indices));
+	}
+	
 	// Once the mesh data is finished loading, set up the required buffers &
 	//   vertex array with OpenGL
 	SetupVertexArray();
@@ -70,23 +94,26 @@ void Mesh::LoadTexture(const char* filename, const TextureOptions& options) {
 }
 
 void Mesh::Render() {
+	// TODO: add a check to see if the texture is valid / has been loaded
 	texture.Bind();
 	// Start with the VAO, which has all of the buffer/attribute settings
 	glBindVertexArray(vertexArrayID);
-	// Then, draw the mesh using indexed drawing & the element buffer
-	glDrawElements(GL_TRIANGLES, elementBuffer.size(), GL_UNSIGNED_INT, 0);
-	// ^ 1: the primitive type (just like the VBO DrawArrays version)
-	//   2: # of elements to draw (we have 6 indices, so draw 6 vertices
-	//   3: the type of the indices
-	//   4: offset or array ref, we don't need to worry abt it now
-
-	// If drawing straight from the VBO (w/out indexed drawing) is desired, use this:
-	/*
-	  glDrawArrays(GL_TRIANGLES, 0, 3);
-	  // ^ 1: the primitive type that we want to draw
-	  //   2: The starting index of the vertex array that we want to draw
-	  //   3: How many vertices we want to draw
-	*/
+	
+	if (useEBO) {
+		// Draw the mesh using indexed drawing & the element buffer
+		glDrawElements(GL_TRIANGLES, elementBuffer.size(), GL_UNSIGNED_INT, 0);
+		// ^ 1: the primitive type (just like the VBO DrawArrays version)
+		//   2: # of elements to draw (we have 6 indices, so draw 6 vertices
+		//   3: the type of the indices
+		//   4: offset or array ref, we don't need to worry abt it now
+	}
+	else {
+		// Draw straight from the VBO (w/out indexed drawing)
+		glDrawArrays(GL_TRIANGLES, 0, vertexBuffer.size() / 5);
+		// ^ 1: the primitive type that we want to draw
+		//   2: The starting index of the vertex array that we want to draw
+		//   3: How many vertices we want to draw
+	}
 }
 
 void Mesh::SetupVertexArray() {
@@ -100,17 +127,18 @@ void Mesh::SetupVertexArray() {
 	glBindVertexArray(vertexArrayID);
 
 	/* ----- Create the buffer objects ----- */
-	//   Note: you could create all 3 at once, but for readability I'll split it up
-	// Create VBOs - Big buffers for sending a bunch of data to the GPU at once
-	glGenBuffers(1, &positionBufferID);
-	glGenBuffers(1, &colorBufferID);
-	glGenBuffers(1, &texCoordBufferID);
-	// Create the EBO - a way to specify both the vertices, and the order to draw them.
-	//   This lets you do things like draw a rectangle by only specifying 4 points
-	//   (otherwise, you'd have to define two triangles: 6 points,
-	//   2 of them identical)
-	glGenBuffers(1, &elementBufferID);
+	// Main VBO
+	glGenBuffers(1, &vertexBufferID);
+	// Optional EBO
+	if (useEBO) {
+		// Create the EBO - a way to specify both the vertices, and the order to draw them.
+		//   This lets you do things like draw a rectangle by only specifying 4 points
+		//   (otherwise, you'd have to define two triangles: 6 points,
+		//   2 of them identical)
+		glGenBuffers(1, &elementBufferID);
 
+	}
+	
 	/* ----- Send the buffers to the GPU and set their attribute pointers ----- */
 	// GENERAL EXPLANATION & NOTES:
 	// glBindBuffer - Selects a buffer as the current VBO for OpenGL to modify
@@ -143,40 +171,30 @@ void Mesh::SetupVertexArray() {
 	//     we call glVertexAttribPointer
 	// glEnableVertexAttribArray - Tell OpenGL there is an attribute at this location
 
-	// Position Data
-	glBindBuffer(GL_ARRAY_BUFFER, positionBufferID);
-	glBufferData(GL_ARRAY_BUFFER, positionBuffer.size() * sizeof(GLfloat), 
-	             &positionBuffer[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Send vertex data to the GPU
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, vertexBuffer.size() * sizeof(GLfloat), 
+	             &vertexBuffer[0], GL_STATIC_DRAW);
+	// Setup the position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
 	glEnableVertexAttribArray(0);
-	// If there was more data attached to the same buffer, we'd add attribute
-	//   pointers here with a different offset & stride
-	// ex: glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-
-	// Color Data
-	glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
-	glBufferData(GL_ARRAY_BUFFER, colorBuffer.size() * sizeof(GLfloat),
-	             &colorBuffer[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Setup the texCoord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
+	                      (void*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	// Texture Coordinate Data
-	glBindBuffer(GL_ARRAY_BUFFER, texCoordBufferID);
-	glBufferData(GL_ARRAY_BUFFER, texCoordBuffer.size() * sizeof(GLfloat),
-	             &texCoordBuffer[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (void*)0);
-	glEnableVertexAttribArray(2);
-
-	// Element Buffer
-	// From now on, binding to the VAO will also bind to this EBO, Letting us use
-	//   glDrawElements instead of glDrawArrays for this mesh
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer.size() * sizeof(GLfloat),
-	             &elementBuffer[0], GL_STATIC_DRAW);
-	// EBO's don't have attributes
-	// IMPORTANT NOTE: a VAO can only have 1 EBO. The VAO uses whatever EBO was
-	//   most recently bound
-
+	if (useEBO) {
+		// Element Buffer
+		// From now on, binding to the VAO will also bind to this EBO, Letting us use
+		//   glDrawElements instead of glDrawArrays for this mesh
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer.size() * sizeof(GLfloat),
+			&elementBuffer[0], GL_STATIC_DRAW);
+		// EBO's don't have attributes
+		// IMPORTANT NOTE: a VAO can only have 1 EBO. The VAO uses whatever EBO was
+		//   most recently bound
+	}
+	
 	/* ----- (Optional) Unbind the Buffers & Vertex Array Object ----- */
 	// This prevents further calls from accidentally modifying these objects
 	// You can unbind the VBO's, since they're already registered with the VAO
--- src/Mesh.h
+++ src/Mesh.h
@@ -15,7 +15,7 @@ public:
 	~Mesh();
 
 	// Load mesh data into buffer vectors, but don't create the opengl buffer objects
-	void LoadMesh(const char* filename);
+	void LoadMesh(const char* filename, const bool use_EBO = false);
 	// Create the texture object that this mesh will use
 	void LoadTexture(const char* filename, const TextureOptions& options);
 	// Draw this mesh onto the screen
@@ -27,23 +27,17 @@ private:
 	// Vertex Array Object - holds the mappings between buffers and attributes
 	GLuint vertexArrayID;
 
-	// Element Buffer Object - Specifies which vertices to use for each triangle
+	// Vertex Buffer Objects - Stores raw vertex data
+	// Note: all vertex data is currently stored in a single VBO. This is good when
+	//   vertex data doesn't change, but if position data changes often (i.e. cloth sim),
+	//   then keep position/normal & other data in separate VBOs
+	std::vector<GLfloat> vertexBuffer;
+	GLuint vertexBufferID;
+
+	// Element Buffer Object (optional) - Specifies which vertices to use for each triangle
+	bool useEBO;
 	std::vector<GLuint> elementBuffer;
 	GLuint elementBufferID;
-
-	// Vertex Buffer Objects - Stores raw vertex data
-	// Note: each part of the mesh data is stored in a separate VBO. This is non-optimal,
-	//   since they're no longer stored in a single contigous memory space. But it allows
-	//   updating some parameters on-the-fly (i.e. position & normal) without having to
-	//   re-send a single massive VBO. If no dynamic geometry is needed, it's better
-	//   to keep everything in one VBO and just be careful when setting attributes.
-	std::vector<GLfloat> positionBuffer;
-	GLuint positionBufferID;
-	// TODO: color probably shouldn't be stored on a point-by-point basis
-	std::vector<GLfloat> colorBuffer;
-	GLuint colorBufferID;
-	std::vector<GLfloat> texCoordBuffer;
-	GLuint texCoordBufferID;
 
 	// Texture associated with this mesh
 	// Note: for now, every object can have only 1 Texture, and textures cannot be
