f96376e0f4f32fde92869bc909abde0124a21a21
Jonah Taylor <jonahtaylor@att.net>
2021-11-28 21:10:16 -0600

    Create helper class for storing transform data

---
+++ src/Transform.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+/// 
+/// Helper class for defining transforms and misc transform operations
+///
+class Transform {
+public:
+	Transform() :
+		loc(0.0f, 0.0f, 0.0f),
+		rot(0.0f, 0.0f, 0.0f),
+		scale(1.0f, 1.0f, 1.0f) {}
+	Transform(glm::vec3 in_loc, glm::vec3 in_rot, glm::vec3 in_scale) :
+		loc(in_loc), rot(in_rot), scale(in_scale) {}
+	~Transform() = default;
+
+	glm::mat4 GetMatrix() {
+		glm::mat4 M(1.0f);
+		M = glm::translate(M, loc);
+		M = glm::rotate(M, rot.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		M = glm::rotate(M, rot.y, glm::vec3(0.0f, 1.0f, 0.0f));
+		M = glm::rotate(M, rot.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		M = glm::scale(M, scale);
+		return M;
+	}
+
+	// loc, rot, and scale w.r.t a parent object
+	glm::vec3 loc;
+	// TODO: convert all euler rots to quats
+	glm::vec3 rot;
+	glm::vec3 scale;
+private:
+};
