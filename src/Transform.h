fef827e402b3a0714f8ce36bdb36bb865181b4fd
JonahT22 <jonahtaylor@att.net>
2021-11-29 17:21:07 -0600

    Store rotations as quaternions

--- src/Transform.h
+++ src/Transform.h
@@ -4,34 +4,44 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 /// 
 /// Helper class for defining transforms and misc transform operations
 ///
 class Transform {
 public:
+	inline static glm::quat EulerToQuat(const glm::vec3 euler) {
+		glm::quat q_x = glm::angleAxis(euler.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		glm::quat q_y = glm::angleAxis(euler.y, glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::quat q_z = glm::angleAxis(euler.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		return q_x * q_y * q_z;
+	}
+
 	Transform() :
 		loc(0.0f, 0.0f, 0.0f),
-		rot(0.0f, 0.0f, 0.0f),
+		// Note: quat constructor is (w, x, y, z)
+		rot(1.0f, 0.0f, 0.0f, 0.0f),
 		scale(1.0f, 1.0f, 1.0f) {}
-	Transform(glm::vec3 in_loc, glm::vec3 in_rot, glm::vec3 in_scale) :
+	Transform(glm::vec3 in_loc, glm::quat in_rot, glm::vec3 in_scale) :
 		loc(in_loc), rot(in_rot), scale(in_scale) {}
+	Transform(glm::vec3 in_loc, glm::vec3 in_rot, glm::vec3 in_scale) :
+		loc(in_loc), rot(EulerToQuat(in_rot)), scale(in_scale) {}
 	~Transform() = default;
 
 	glm::mat4 GetMatrix() {
+		// TODO: this needs to be tested
 		glm::mat4 M(1.0f);
 		M = glm::translate(M, loc);
-		M = glm::rotate(M, rot.x, glm::vec3(1.0f, 0.0f, 0.0f));
-		M = glm::rotate(M, rot.y, glm::vec3(0.0f, 1.0f, 0.0f));
-		M = glm::rotate(M, rot.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		M *= glm::mat4_cast(rot);
 		M = glm::scale(M, scale);
+		std::cerr << "Warning: Transform::GetMatrix() has not been tested" << std::endl;
 		return M;
 	}
 
 	// loc, rot, and scale w.r.t a parent object
 	glm::vec3 loc;
-	// TODO: convert all euler rots to quats
-	glm::vec3 rot;
+	glm::quat rot;
 	glm::vec3 scale;
 
 	friend std::ostream& operator<<(std::ostream& os, const Transform& t);
@@ -40,7 +50,7 @@ private:
 
 inline std::ostream& operator<<(std::ostream& os, const Transform& t) {
 	os << "Loc: (" << t.loc.x << ", " << t.loc.y << ", " << t.loc.z << ")\n";
-	os << "Rot: (" << t.rot.x << ", " << t.rot.y << ", " << t.rot.z << ")\n";
+	os << "Rot: (" << t.rot.x << ", " << t.rot.y << ", " << t.rot.z << ", " << t.rot.w << ")\n";
 	os << "Scale: (" << t.scale.x << ", " << t.scale.y << ", " << t.scale.z << ")\n";
 	return os;
 }
