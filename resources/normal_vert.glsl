cedfde6427e56a30628799f6747d71accf2de90e
JonahT22 <jonahtaylor@att.net>
2021-12-02 14:00:12 -0600

    Rename simple shader -> unlit, and add starter for normal shader

--- resources/normal_vert.glsl
+++ resources/normal_vert.glsl
@@ -3,7 +3,8 @@
 
 /* ----- Attributes ----- */
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout (location = 1) in vec3 aNor;
+layout (location = 2) in vec2 aTexCoord;
 
 /* ----- Uniforms ----- */
 // Model, view, and projection matrices
---
+++ resources/unlit_vert.glsl
@@ -0,0 +1,25 @@
+// Opengl version 3.3, using the core profile
+#version 330 core
+
+/* ----- Attributes ----- */
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNor;
+layout (location = 2) in vec2 aTexCoord;
+
+/* ----- Uniforms ----- */
+// Model, view, and projection matrices
+uniform mat4 M;
+uniform mat4 V;
+uniform mat4 P;
+
+/* ----- Outputs ----- */
+out vertexInfo {
+	vec2 texCoord;
+};
+
+
+void main() {
+	// gl_Position is a built-in variable, and the final output of a vertex shader
+	gl_Position = P * V * M * vec4(aPos, 1.0);
+	texCoord = aTexCoord;
+}
