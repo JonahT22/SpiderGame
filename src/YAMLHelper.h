e0ca0d5a70f7785b2e18122cb1bf6c61d1f7c65d
JonahT22 <jonahtaylor@att.net>
2021-12-05 01:10:38 -0600

    Parse transform objects in YAML files

--- src/YAMLHelper.h
+++ src/YAMLHelper.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <string>
+
 #include <glm/glm.hpp>
 #include <yaml-cpp/yaml.h>
+
+#include "Transform.h"
 
 ///
 /// Various helper functions for working with YAML files
@@ -17,7 +21,7 @@ struct convert<glm::vec3> {
 		node.push_back(rhs.z);
 		return node;
 	}
-
+	// Reading vec3's from YAML files
 	static bool decode(const Node& node, glm::vec3& rhs) {
 		if (!node.IsSequence() || node.size() != 3) {
 			return false;
@@ -26,6 +30,32 @@ struct convert<glm::vec3> {
 		rhs.x = node[0].as<float>();
 		rhs.y = node[1].as<float>();
 		rhs.z = node[2].as<float>();
+		return true;
+	}
+};
+
+// Custom parsing for the Transform class
+template<>
+struct convert<Transform> {
+	// Note: There is NO encoding function defined to write Transforms to YAML files.
+	//   There is no conversion written (yet) from quat -> euler, but YAML files are
+	//   written w/euler angles for readability. To be implemented later if necessary
+
+	// Read transform from a YAML file, given in this format:
+	//     location: [x,y,z]
+	//     rotation: [x,y,z]
+	//     scale: [x,y,z]
+	static bool decode(const Node& node, Transform& rhs) {
+		if (!node.IsMap()) {
+			return false;
+		}
+		if (!node["location"] || !node["rotation"] || !node["scale"]) {
+			return false;
+		}
+
+		rhs.loc = node["location"].as<glm::vec3>();
+		rhs.rot = Transform::EulerToQuat(node["rotation"].as<glm::vec3>());
+		rhs.scale = node["scale"].as<glm::vec3>();
 		return true;
 	}
 };
